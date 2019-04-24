141209f649410466e24530e814a6d01fd0412765
mark <Samark.Cha@ascendcorp.com>
2019-04-24 18:16:01 +0700

    fixed

--- clients/printer.php
+++ clients/printer.php
@@ -1,4 +1,5 @@
 <?php
 #header( "location: quickprinter://test" );
-header( "location: intent://#Intent;scheme=quickprinter;package=pe.diegoveloper.printerserverapp;end;" );
+header( 'refresh: 2; url=intent://#Intent;scheme=quickprinter;package=pe.diegoveloper.printerserverapp;end;');
+#header( "location: intent://#Intent;scheme=quickprinter;package=pe.diegoveloper.printerserverapp;end;" );
 exit(0);
